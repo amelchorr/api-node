86403014412a28cb1adb28561723f5c25bb5b21d
Armando Melchor Revilla <amelchoripera4@gmail.com>
2022-10-19 00:43:51 -0500

    Se realizan adecuaciones a script de limpieza

--- destroy.sh
+++ destroy.sh
@@ -3,6 +3,12 @@
 echo '==========================='
 echo '=== ARCHIVO DE LIMPIEZA ==='
 echo '==========================='
+
+echo 'Paso 1.- Limpieza en Contenedores'
 docker rm -fv mongodb
 docker network rm devops-net 
 docker image rm mongo
+
+echo 'Paso 2.- Limpieza en Directorios'
+rm -rRf ./database-data
+rm -rRf ./database-key
